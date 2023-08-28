bef0962d6e9062e3f3a6757103430d7cc134554e
Jordan <118126508+simplethomas@users.noreply.github.com>
2023-08-27 20:08:38 -0400

    Choose sketchboard size with slider

--- javascript.js
+++ javascript.js
@@ -1,17 +1,59 @@
 const container = document.querySelector('#container');
+let squareSize = 16
+
+
+function createDivs() {
+    let totalDivs = squareSize * squareSize
+    for (let i = 0; i < totalDivs; i++) {
+    container.appendChild(createDiv(i));
+}}
 
 
 function createDiv(index) {
     let animationClass = Math.floor(Math.random() * 5) + 1;
     let div = document.createElement('div');
-    div.class = 'box' + animationClass;
+    div.classList.add('squares');
     div.id = index;
     return div;
 }
 
 
-
-for (let i = 0; i < 16; i++) {
-    container.appendChild(createDiv(i));
+function changeGridSize() {
+    container.style.gridTemplateColumns = `repeat(${squareSize}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${squareSize}, 1fr)`
 }
 
+
+//Clear grid with button//
+document.querySelector('#button').addEventListener('click', function() {
+    document.querySelectorAll('.hovered').forEach(item => {
+        item.classList.toggle('hovered')
+        });
+    });
+
+    
+
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value
+    squareSize = this.value
+    document.querySelectorAll('.squares').forEach(item => {
+        item.remove()
+    });
+    createDivs()
+    changeGridSize()
+    changeBackground()
+    console.log(squareSize)
+};
+
+//Change background colors of each div to black when mouse enters the div//
+function changeBackground() {
+    document.querySelectorAll('.squares').forEach(item => {
+    item.addEventListener('mouseover', function(e) {
+        e.target.classList.add('hovered');
+    });
+});
+}
